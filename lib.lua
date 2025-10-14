5648994586895b6b3a1ab549dc7657e1de99b772
randomahhnamecauseimtoolazy <crobster.lol@gmail.com>
2025-10-14 10:15:25 +0200

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -3023,13 +3023,14 @@ do
     })
 
     local WatermarkOuter = Library:Create('Frame', {
-        BorderColor3 = Color3.new(0, 0, 0),
-        Position = UDim2.new(0, 100, 0, -25),
-        Size = UDim2.new(0, 213, 0, 20),
-        ZIndex = 200,
-        Visible = false,
-        Parent = ScreenGui,
-    })
+    AnchorPoint = Vector2.new(1, 1),
+    BorderColor3 = Color3.new(0, 0, 0),
+    Position = UDim2.new(1, -1, 1, -1),
+    Size = UDim2.new(0, 213, 0, 20),
+    ZIndex = 200,
+    Visible = false,
+    Parent = ScreenGui,
+})
 
     local WatermarkInner = Library:Create('Frame', {
         BackgroundColor3 = Library.MainColor,
